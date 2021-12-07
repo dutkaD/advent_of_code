21706d96a4f65f87fda437ae9f579cdcddb7be91
Diana Dutka <diana.dutka@gmail.com>
2021-12-07 10:57:40 +0100

    day 6

--- day6/main/main.go
+++ day6/main/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"advent_of_code/day6"
 	"advent_of_code/utils"
+	"fmt"
 )
 
 func main() {
-	lines := utils.ReadFile("../input.txt")
-	_ = lines
-
-	// part 1
-
-	// part 2
+	lines := utils.ReadFile("input.txt")
+	// part 1 / 2
+	fmt.Println(day6.GetFishCount(lines))
 }
